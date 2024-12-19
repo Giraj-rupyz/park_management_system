62669b36331ae3b886a47150dc8f5c2ce2e482d1
Giraj-rupyz <giraj.neema@rupyz.com>
2024-12-19 19:18:27 +0530

    Add files via upload
    fixed print statement

--- park_management_system.py
+++ park_management_system.py
@@ -23,6 +23,8 @@ class park_management():
         self.price_for_teenagers = price_for_teenagers
         self.price_for_children = price_for_children
 
+
+
     # function for booking
 
     def book_ticket(self, name, age, phone, gender):
@@ -45,14 +47,14 @@ class park_management():
                 price_before_discount = self.price_for_children
                 price_after_discount = price_before_discount-((self.discount_for_children/100)*price_before_discount)
 
-        print(f"Price to be paid {price_after_discount}")
-        print()
-        print("******Processing payment*******")
-        print()
-        print("Payment Successful! Welcome to the park ")
-        print()
-        print()
-        print()
+        print(f"Price to be paid {price_after_discount} \n")
+        # print()
+        print("******Processing payment******* \n")
+        # print()
+        print("Payment Successful! Welcome to the park \n\n\n")
+        # print()
+        # print()
+        # print()
         self.visitors.append(visitors_map)
 
     # function to display prices
@@ -60,26 +62,26 @@ class park_management():
     def display_prices(self):
         print(f"price for people below 10 years: {self.price_for_children}")
         print(f"Price for people between 10 to 20 years: {self.price_for_teenagers}")
-        print(f"Price for people above 20 years: {self.price_for_adults}")
-        print()
-        print()
-        print()
+        print(f"Price for people above 20 years: {self.price_for_adults}\n\n\n")
+        # print()
+        # print()
+        # print()
 
     # function to display discounts
     def display_discounts(self):
         print(f"There is a discount of {self.discount_for_men}% for all mens")
         print(f"There is a discount of {self.discount_for_women}% for all women")
-        print(f"There is a discount of {self.discount_for_children}% for all children below 20")
-        print()
-        print()
-        print()
+        print(f"There is a discount of {self.discount_for_children}% for all children below 20 \n\n\n")
+        # print()
+        # print()
+        # print()
 
     # function to see number of visitors
     def visitors_count(self):
-        print(f"Total visitors in the park: {len(self.visitors)}")
-        print()
-        print()
-        print()
+        print(f"Total visitors in the park: {len(self.visitors)} \n\n\n")
+        # print()
+        # print()
+        # print()
 
 
 def main():
@@ -91,52 +93,52 @@ def main():
     # user panel
 
     while True:
-        print("1. booking", end="      ")
-        print("2. view prices", end="       ")
-        print("3. view offers", end="       ")
-        print("4. see number of visitors", end="        ")
+        print("1. booking\t\t", end="")
+        print("2. view prices\t\t", end="")
+        print("3. view offers\t\t", end="")
+        print("4. see number of visitors\t\t", end="")
         print("5. exit")
         user_choice = int(input("Enter your choice "))
-        print()
-        print()
-        print()
+        print("\n\n\n")
+        # print()
+        # print()
 
         if user_choice == 1:
             name = input("Enter your name: ")
-            age = int(input("Enter your age: "))
+            age = int(input("Enter your age:"))
             if age < 0:
                 print()
-                print("Enter valid age")
-                print()
-                print("Enter your details again")
-                print()
-                print()
-                print()
+                print("Enter valid age \n")
+                # print()
+                print("Enter your details again \n\n\n")
+                # print()
+                # print()
+                # print()
                 continue
 
             phone = input("Enter your phone number: ")
             if len(phone) != 10:
                 print()
-                print("Enter a valid phone number")
-                print()
-                print("Enter your details again")
-                print()
-                print()
-                print()
+                print("Enter a valid phone number\n")
+                # print()
+                print("Enter your details again \n\n\n")
+                # print()
+                # print()
+                # print()
                 continue
-            gender = input("Enter your gender (male/female): ")
+            gender = input("Enter your gender (male/female):")
             if gender == 'male' or gender == 'female':
                 print()
                 park_system.book_ticket(name, age, phone, gender.lower())
 
             else:
                 print()
-                print("Please enter your gender in the given format only")
-                print()
-                print("Enter your details again")
-                print()
-                print()
-                print()
+                print("Please enter your gender in the given format only \n")
+                # print()
+                print("Enter your details again \n\n\n")
+                # print()
+                # print()
+                # print()
                 continue
 
         elif user_choice == 2:
